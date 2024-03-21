daaea145937ba85145a52b250359dc2ee0c52a25
RMehdid <samy.mehdid@yassir.com>
2024-03-21 10:08:14 +0100

    Initial Commit

---
+++ WinkyKiller/WinkyKillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinkyKillerApp.swift
+//  WinkyKiller
+//
+//  Created by Samy Mehdid on 21/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct WinkyKillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
