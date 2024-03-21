d237c3ee2db3438cf87d2ab01b7af4548ecf90bb
RMehdid <samy.mehdid@yassir.com>
2024-03-21 15:28:36 +0100

    work on playersNumView

--- WinkyKiller/Views/PlayersNumView.swift
+++ WinkyKiller/Views/PlayersNumView.swift
@@ -14,21 +14,33 @@ struct PlayersNumView: View {
     @FocusState private var playersNumFieldIsFocused: Bool
     
     var body: some View {
-        VStack(spacing: 38){
-            Text("How many are you?")
-                .font(.custom("Amarante-Regular", size: 32))
+        VStack{
+            Spacer()
+            VStack(spacing: 38){
+                Text("How many are you?")
+                    .font(.custom("Amarante-Regular", size: 32))
+                
+                TextField("Enter here", text: $playersNum)
+                    .focused($playersNumFieldIsFocused)
+                    .fixedSize(horizontal: true, vertical: true)
+                    .padding(4)
+                    .padding(.horizontal, 8)
+                    .background(.ultraThinMaterial)
+                    .clipShape(.rect(cornerRadius: 4))
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.ultraThinMaterial)
+            .clipShape(.rect(cornerRadius: 16))
             
-            TextField("Enter here", text: $playersNum)
-                .focused($playersNumFieldIsFocused)
-                .fixedSize(horizontal: true, vertical: true)
-                .padding(4)
-                .padding(.horizontal, 8)
-                .background(.ultraThinMaterial)
-                .clipShape(.rect(cornerRadius: 4))
+            Spacer()
+            
+            NavigationLink("Confirm") {
+                RoleCard(Required.killer)
+            }
+            .buttonStyle(PrimaryButton())
         }
         .padding()
-        .background(.ultraThinMaterial)
-        .clipShape(.rect(cornerRadius: 16))
     }
 }
 
